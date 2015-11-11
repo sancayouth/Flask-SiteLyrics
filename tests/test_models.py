cf07eb6ade2eb8c858967bad88281700dc08e598
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-11-11 02:35:19 -0300

    Add some tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,6 +29,11 @@ class ModelsTestCase(BaseTestCase):
         a1 = Artist.query.filter_by(name='INTERPOL').first()
         self.assertIsNone(a1)
 
+    def test_artist_name_withoutspace(self):
+        user = User.query.filter_by(username='admin').first()
+        a1 = Artist('BOOM BOOM KID', user.get_id())
+        self.assertEqual('BOOMBOOMKID', a1.get_name_without_spaces())
+
     def test_albums_created(self):
         user = User.query.filter_by(username='admin').first()
         a1 = Artist('ARTIST', user.get_id())
@@ -40,9 +45,10 @@ class ModelsTestCase(BaseTestCase):
         db.session.commit()
         self.assertEqual(3, len(Album.query.all()))
 
-    def test_delete_albums(self):
+    def test_albums_deleted(self):
         user = User.query.filter_by(username='admin').first()
         a1 = Artist('ARTIST', user.get_id())
+        a2 = Artist('THE ARTIST', user.get_id())
         db.session.add(a1)
         al1 = Album('album 1', user.get_id(), a1.get_id())
         al2 = Album('album 2', user.get_id(), a1.get_id())
@@ -55,6 +61,35 @@ class ModelsTestCase(BaseTestCase):
         self.assertEqual(2, len(Album.query.all()))
         al1 = Album.query.filter_by(name='album 1').first()
         self.assertIsNone(al1)
+
+    def test_album_name_withoutspace(self):
+        user = User.query.filter_by(username='admin').first()
+        a1 = Artist('ARTIST', user.get_id())
+        db.session.add(a1)
+        al1 = Album('album 1', user.get_id(), a1.get_id())
+        self.assertEqual('ALBUM1', al1.get_name_without_spaces())
+
+    def test_albums_are_deleted_when_artist_are_deleted(self):
+        user = User.query.filter_by(username='admin').first()
+        a1 = Artist('ARTIST', user.get_id())
+        a2 = Artist('ARTIST2', user.get_id())
+        db.session.add_all([a1, a2])
+        db.session.commit()
+        al1 = Album('album 1', user.get_id(), a1.get_id())
+        al2 = Album('album 2', user.get_id(), a1.get_id())
+        al3 = Album('album 3', user.get_id(), a1.get_id())
+        al1b = Album('album 1_b', user.get_id(), a2.get_id())
+        al2b = Album('album 2_b', user.get_id(), a2.get_id())
+        al3b = Album('album 3_b', user.get_id(), a2.get_id())
+        db.session.add_all([al1, al2, al3, al1b, al2b, al3b])
+        db.session.commit()
+        db.session.delete(a1)
+        db.session.commit()
+        al1 = Album.query.filter_by(name='album 1').first()
+        self.assertIsNone(al1)
+        al1b = Album.query.filter_by(name='album 1_b').first()
+        self.assertIsNotNone(al1b)
+        self.assertEqual(3, len(Album.query.all()))
 
 if __name__ == '__main__':
     unittest.main()
