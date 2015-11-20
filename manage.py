6427f655cd36a22ed1b59a97440dc723ccefe6d4
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-11-20 02:40:54 -0300

    Add method filldb to manage

--- manage.py
+++ manage.py
@@ -2,15 +2,17 @@ from flask.ext.script import Manager
 from app import create_app
 from app.extensions import db
 from config import DevelopmentConfig
-from app.models import User
+from app.models import User, Artist
 
 
 app = create_app(DevelopmentConfig)
 manager = Manager(app)
 
+
 @manager.command
 def run():
     app.run()
+
 
 @manager.command
 def adduser(email, username):
@@ -25,6 +27,15 @@ def adduser(email, username):
     db.session.add(user)
     db.session.commit()
 
+@manager.command
+def filldb():
+    usr = User.query.get(1)
+    artists = []
+    for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+        for j in range(1, 200):
+            artists.append(Artist(i +' ARTIST '+ str(j), usr.get_id()))
+    db.session.add_all(artists)
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
