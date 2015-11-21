43b12d7cc2f6a685d99c339845018f2fb3bfed01
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-11-21 02:20:03 -0300

    Add method to fill db

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 from flask.ext.script import Manager
 from app import create_app
 from app.extensions import db
+from app.models import User, Artist, Album, Lyric
 from config import DevelopmentConfig
-from app.models import User, Artist
+from datetime import datetime
 
 
 app = create_app(DevelopmentConfig)
@@ -27,15 +28,51 @@ def adduser(email, username):
     db.session.add(user)
     db.session.commit()
 
+
 @manager.command
 def filldb():
     usr = User.query.get(1)
     artists = []
     for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
         for j in range(1, 200):
-            artists.append(Artist(i +' ARTIST '+ str(j), usr.get_id()))
+            artists.append(Artist(i + ' ARTIST ' + str(j), usr.get_id()))
     db.session.add_all(artists)
     db.session.commit()
+    arts = Artist.query.all()
+    albums = []
+    for artist in arts:
+        for i in range(1, 3):
+            albums.append(
+                Album('ALBUM ' + str(i), datetime.now(),
+                    usr.get_id(), artist.get_id()))
+    db.session.add_all(albums)
+    db.session.commit()
+    albs = Album.query.all()
+    lyrics = []
+    song_ly = ''' nananananana
+                  nanananananan
+                  nananananana
+                  nananananana
+
+                  nananananana
+                  nanananananan
+                  nananananana
+                  nananananana
+
+                  batman batman'''
+    for album in albs:
+        for i in range(1, 8):
+            lyrics.append(
+                Lyric(
+                    'NANANA batman ' + str(i),
+                    song_ly,
+                    usr.get_id(),
+                    album.get_id()
+                )
+            )
+    db.session.add_all(lyrics)
+    db.session.commit()
+
 
 if __name__ == '__main__':
     manager.run()
