f08cd0c7812b93056d05c875df3e437792d82d1a
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-11-22 04:20:54 -0300

    Add several changes

--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, redirect, url_for
-from app.models import Artist
+from app.models import Artist, Lyric
 
 home = Blueprint('home', __name__, template_folder='templates')
 
@@ -10,7 +10,7 @@ def home_():
     return render_template('index.html', title='Flask Lyrics')
 
 
-@home.route('/<letter>')
+@home.route('/artists/<letter>')
 def artists_by_letter(letter):
     if  len(letter) == 1 and letter.isalpha():
         artists = Artist.get_artists_by_letter(letter)
@@ -23,4 +23,25 @@ def artists_by_letter(letter):
                 right.append(artist)
         return render_template('artists.html', title='Flask Lyrics - Artists',
                                 left=left, right=right, letter=letter)
+    return redirect(url_for('home.home_'))
+
+@home.route('/artist/<artist_ws>')
+def albums_by_artist(artist_ws):
+    artist = Artist.query.filter_by(name_ws=artist_ws).first()
+    if artist:
+        return render_template(
+                    'artist.html',
+                    title='Flask Lyrics - ' + artist.name, artist=artist)
+    return redirect(url_for('home.home_'))
+
+@home.route('/lyrics/<artist_ws>/<int:lyric_id>')
+def lyric_by_artist(artist_ws, lyric_id = None):
+    artist = Artist.query.filter_by(name_ws=artist_ws).first()
+    if artist and lyric_id:
+        lyric = Lyric.query.filter_by(id=lyric_id).first()
+        if lyric:
+            return render_template(
+                        'lyric.html',
+                        title='Flask Lyric - ' + lyric.name, artist=artist,
+                        lyric=lyric)
     return redirect(url_for('home.home_'))
